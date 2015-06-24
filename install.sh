4ddd3e2ffb12c3e38b855b99766cc8f39340dd58
wolfitem <wei.wu86@hotmail.com>
2015-06-24 23:27:16 +0800

    add install docker shell

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #install docker
-
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo ln	-sf /usr/bin/docker.io /usr/local/bin/docker
+sudo sed -i '$acomplete	-F _docker docker' /etc/bash_completion.d/docker.io
 
 
 mkdir -p /usr/local/docker/workspace/android
