b68bfd22566a619f260a5f7158005143eecf824a
wolfitem <wei.wu86@hotmail.com>
2015-07-16 01:53:59 +0800

    update install docker shell

--- install.sh
+++ install.sh
@@ -4,9 +4,8 @@
 command -v docker >/dev/null 2>&1 || { 
 	echo >&2 "Install docker..."; 
 	sudo apt-get update
-	sudo apt-get install -y docker.io
-	sudo ln	-sf /usr/bin/docker.io /usr/local/bin/docker
-	sudo sed -i '$acomplete	-F _docker docker' /etc/bash_completion.d/docker.io
+	sudo apt-get install wget
+	wget -qO- https://get.docker.com/ | sh
 }
 
 mkdir -p workspace/android
