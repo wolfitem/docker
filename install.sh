219c0ad1fbf61ef3e003999db71eaa46e7ae6cae
wolfitem <wei.wu86@hotmail.com>
2015-08-03 16:33:49 +0800

    add install nsenter shell

--- install.sh
+++ install.sh
@@ -8,6 +8,20 @@ command -v docker >/dev/null 2>&1 || {
 	wget -qO- https://get.docker.com/ | sh
 }
 
+#install nsenter
+command -v nsenter >/dev/null 2>&1 || {
+	echo >&2 "Install nsenter...";
+	cd /tmp
+	wget https://www.kernel.org/pub/linux/utils/util-linux/v2.24/util-linux-2.24.tar.gz
+	tar -xzvf util-linux-2.24.tar.gz
+	cd util-linux-2.24
+	./configure --without-ncurses
+	make nsenter
+	sudo cp nsenter	/usr/local/bin
+	cd ..
+	rm -rf /tmp/util-linux-2.24.tar.gz && rm -rf /tmp/util-linux-2.24 
+}
+
 mkdir -p workspace/android
 mkdir -p workspace/php
 mkdir -p workspace/soapui
@@ -18,7 +32,7 @@ mkdir -p config/android-studio
 
 USER=$(whoami)
 sudo chown -R $USER:$USER $PWD
-echo 'set DOCKER_ROOT='$PWD >> ~/.bashrc
+echo 'DOCKER_ROOT='$PWD >> ~/.bashrc
 #add short command
 
 if [ -f alias ]; then
